e9482ed63294cffe28ab58b63f4eae704806cfca
GildedBirch <150861088+GildedBirch@users.noreply.github.com>
2024-12-21 20:19:32 +0200

    Delete scripts directory

--- scripts/card.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name Card
-
-@export var display_name: String = ""
-@export var picture: Texture2D
-@export_multiline var description: String = ""
-
-enum BG_COLOR {GREY, BLACK, BLUE}
-@export var bg_color: BG_COLOR = BG_COLOR.GREY
